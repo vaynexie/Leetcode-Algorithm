675123fc139ee53d6983abbb875e7a79ac56f019
Vayne Xie <55047915+vaynexie@users.noreply.github.com>
2020-01-29 19:34:27 +0800

    Update 1_Two_Sum_e.py

--- 1_Two_Sum_e.py
+++ 1_Two_Sum_e.py
@@ -6,3 +6,13 @@ class Solution(object):
             tmp_nums = nums[tmp_nums_start_index:]
             if j in tmp_nums:
                 return [nums.index(i), tmp_nums_start_index + tmp_nums.index(j)]
+
+#better version 
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in nums:
+            j = target - i
+            tmp_nums_start_index = nums.index(i) + 1
+            tmp_nums = nums[tmp_nums_start_index:]
+            if j in tmp_nums:
+                return [nums.index(i), tmp_nums_start_index + tmp_nums.index(j)]

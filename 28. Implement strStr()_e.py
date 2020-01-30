24773122bbd311925ab0c0957992f9f9309f5a5b
Vayne Xie <55047915+vaynexie@users.noreply.github.com>
2020-01-30 17:12:42 +0800

    Update 28. Implement strStr()_e.py

--- 28. Implement strStr()_e.py
+++ 28. Implement strStr()_e.py
@@ -21,3 +21,19 @@ class Solution(object):
             judge=0
             
         return judge
+
+#better solution
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle=='':
+            return 0
+        needle_len=len(needle)
+        for i in range(len(haystack)-needle_len+1):
+            if haystack[i:i+needle_len]==needle:
+                return i
+        return -1

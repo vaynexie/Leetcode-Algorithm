37e3822eb6d81e4efb4b8bd2bd61e3daee61abba
Vayne Xie <55047915+vaynexie@users.noreply.github.com>
2020-01-31 11:26:20 +0800

    Create 58. Length of Last Word_e.py

---
+++ 58. Length of Last Word_e.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s[-1:]==' ':
+            num=0
+            for i in range(len(s)-1,-1,-1):
+                
+                if s[i]!=' ':
+                    break
+                num+=1
+            aa=''
+            for i in range(len(s)-num-1,-1,-1):
+                if s[i]==' ':break
+                aa+=s[i]
+        else:  
+            aa=''
+            for i in range(len(s)-1,-1,-1):
+                if s[i]==' ':break
+                aa+=s[i]
+    
+        return len(aa)
+        
+  #better v
+    index = s[::-1].strip().find(' ')
+    
+    if index != -1:
+        return len(s[len(s) - index:])
+    
+    return len(s.strip())

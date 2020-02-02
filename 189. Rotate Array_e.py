63e5b38d014e683723fb5d5ac2348860c6110ecc
Vayne Xie <55047915+vaynexie@users.noreply.github.com>
2020-02-02 11:47:37 +0800

    Create 189. Rotate Array_e.py

---
+++ 189. Rotate Array_e.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:]=nums[::-1][:k][::-1]+nums[::-1][k:][::-1]
+ 
+#alter v
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[0:-k]
